ebf97c2f3c97341ddcaa4d730044227186517e10
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-12 18:36:04 -0300

    Criação de um array de contas

--- ByteBankArray/Program.cs
+++ ByteBankArray/Program.cs
@@ -3,48 +3,26 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using ByteBankArrays.Modelos.Conta;
 
-namespace ByteBankArray
+namespace ByteBankArrays
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            /* o tipo array é um tipo de referência;
-             * no entanto, ao acessar uma posição do array, estamos pegando o valor contido nessa posição e, por isso, ele pode ser referenciado por uma variável do tipo int;
-             * 
-             */
-            //array de inteiros com 5 posições
-            //int[] idades = null;
-            int [] idades = new int[5];
-            
-            idades[0] = 15;
-            idades[1] = 28;
-            idades[2] = 35;
-            idades[3] = 50;
-            idades[4] = 28;
-            /* Ao excluir/comentar uma posição que existe no array, o retorno será zero (retorno padrão do tipo int)
-             * isso acontece pois o compilador entende que essa posição apenas não foi inicializada
-             */
-
-            int idadeNoIndiceQuatro = idades[4];
-
-            Console.WriteLine(idadeNoIndiceQuatro);
-
-            int[] outroArray = idades;
-            Console.WriteLine(outroArray[3]);
-
-            int acumulador = 0;
-            for (int i = 0; i <= 4; i++)
+            ContaCorrente[] contas = new ContaCorrente[]
             {
-                int idade = idades[i];
-                Console.WriteLine($"Acessando o array idades no índice {i} e obtendo o valor {idade} anos para a idade[{i}]\n");
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 4456668),
+                new ContaCorrente(874, 7781438)
+            };           
 
-                acumulador += idade;
+            for(int i = 0; i < contas.Length; i++)
+            {
+                ContaCorrente contaAtual = contas[i];
+                Console.WriteLine($"Conta {i} {contaAtual.Numero}");
             }
-
-            int media = acumulador / 5;
-            Console.WriteLine($"Média de idades = {media} anos");
 
             Console.ReadKey();
         }
