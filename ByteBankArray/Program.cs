fcfa2fa1b75b12225786ebeaed90e246b1eec330
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-13 11:59:58 -0300

    Teste dos métodos da classe ListaContaCorrente

--- ByteBankArray/Program.cs
+++ ByteBankArray/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using ByteBankArray.SistemaAgencia;
 using ByteBankArrays.Modelos.Conta;
 
 namespace ByteBankArrays
@@ -11,14 +12,30 @@ namespace ByteBankArrays
     {
         static void Main(string[] args)
         {
+            ListaContaCorrente lista = new ListaContaCorrente();
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 4456668));
+            lista.Adicionar(new ContaCorrente(874, 7781438));
+            lista.Adicionar(new ContaCorrente(874, 3379781));
+            lista.Adicionar(new ContaCorrente(874, 1056228));
+            lista.Adicionar(new ContaCorrente(874, 8881432));
+            lista.Adicionar(new ContaCorrente(874, 5679755));
+            lista.Adicionar(new ContaCorrente(874, 3156298));
+            lista.Adicionar(new ContaCorrente(874, 7481266));
+
+            Console.ReadKey();
+        }
+
+        static void TestaArrayContaCorrente()
+        {
             ContaCorrente[] contas = new ContaCorrente[]
             {
                 new ContaCorrente(874, 5679787),
                 new ContaCorrente(874, 4456668),
                 new ContaCorrente(874, 7781438)
-            };           
+            };
 
-            for(int i = 0; i < contas.Length; i++)
+            for (int i = 0; i < contas.Length; i++)
             {
                 ContaCorrente contaAtual = contas[i];
                 Console.WriteLine($"Conta {i} {contaAtual.Numero}");
